383d73bf9352276e9e660a9fb5077787f6574d38
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2023-04-14 14:52:55 +0300

    python comments

---
+++ python-comments.py
@@ -0,0 +1,19 @@
+# Different ways to write comments. This  is a single line comment
+
+print("Hello Alligator") # Any text after the hash is treated as a comment
+
+# This is a 
+# multiline 
+# comment
+
+print("There is also another way to write multiline comments, I will show it below")
+
+"""
+This is also another way
+to write multiline comments.
+Python will ignore any text in string literals so long as it's not assigned to a variable
+
+"""
+
+print("Which method do you like most?")
+
