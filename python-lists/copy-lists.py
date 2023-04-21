e6259867c7c7d7026e1c7516382cb99635d57438
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2023-04-21 15:12:10 +0300

    copying list items

---
+++ python-lists/copy-lists.py
@@ -0,0 +1,7 @@
+cars = ["toyota", "ford", "mercedes", "range rover", "nissan", "suzuki", "tesla"]
+
+firstCarList = cars.copy()
+print(firstCarList)
+
+secondCarList = list(cars)
+print(secondCarList)
